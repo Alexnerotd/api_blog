7c09245283dde5deaf0d7624d7775e6c0214eb0a
Alex nero <jesalemalo@gmail.com>
2024-05-17 16:49:06 -0400

    add connection with database

--- tests/user_test.go
+++ tests/user_test.go
@@ -1,1 +1,13 @@
 package tests
+
+import (
+	"alexnerotd/blog/utils"
+	"testing"
+)
+
+func TestConnectDb(t *testing.T) {
+	_, err := utils.ConnectDB()
+	if err != nil {
+		t.Errorf("Error conectiong to the database: %v", err)
+	}
+}
